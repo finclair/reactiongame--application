884ed777aab92ba4173b0dd5eee2c01ae903977f
Klaus Heino <klaus.heino@gmail.com>
2017-02-17 21:30:38 +0200

    Reset the timer at the end of the function

--- script.js
+++ script.js
@@ -4,20 +4,10 @@ document.getElementById("circle").onclick = function() {
   var endTime = new Date();
   var timeTaken = (endTime - startTime) / 1000;
   var seconds = Math.round(timeTaken % 60)
-
   document.getElementById("timeShown").innerHTML = seconds + "s";
-  console.log(seconds);
-  
- 
+  startTime = endTime;
 }
 
 
-//when user clicks the item the clock starts from zero again
 
-//täytyy ottaa talteen aika kun kuviota klikataan
-//tämän jälkeen tulee 
-
-//jos käyttäjä painaa ajassa 14:15:20
-//ja painaa tämän jälkeen ajassa 14:15:28
-//niin erotus on 8 sekuntia
 
